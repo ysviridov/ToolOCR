2b498c3ec7898c0e057c71c5ee58062c6aca6dea
ysviridov <yuriy.sviridov@gmail.com>
2026-08-25 15:27:42 +0300

    Stage 2.1: подключить web-интерфейс тестирования

--- ocr/app/application.py
+++ ocr/app/application.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from .layout_api import router as layout_router
 from .main import app
+from .test_ui import router as test_ui_router
 
 
 app.include_router(layout_router)
+app.include_router(test_ui_router)
