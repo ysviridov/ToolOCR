fc05f3567c742a11081840ef11452921f467dc35
ysviridov <yuriy.sviridov@gmail.com>
2026-08-26 17:31:24 +0300

    Подключить ROI preview API

--- ocr/app/application.py
+++ ocr/app/application.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from .layout_api import router as layout_router
 from .main import app
+from .roi_test_ui import router as roi_test_ui_router
 from .test_ui import router as test_ui_router
 
 
 app.include_router(layout_router)
 app.include_router(test_ui_router)
+app.include_router(roi_test_ui_router)
