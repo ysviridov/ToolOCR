1c63866db90271d854f766fb1c2b48c0d29f7b70
ysviridov <yuriy.sviridov@gmail.com>
2026-09-03 13:08:58 +0300

    Подключить layout v3 диагностической панели

--- ocr/app/application.py
+++ ocr/app/application.py
@@ -20,6 +20,7 @@ from . import roi_test_ui as _roi_test_ui
 from . import test_ui_postcode_runtime as _test_ui_postcode_runtime
 from . import test_ui_diagnostics as _test_ui_diagnostics  # noqa: F401
 from . import test_ui_diagnostics_refinements as _test_ui_diagnostics_refinements  # noqa: F401
+from . import test_ui_diagnostics_layout_v3 as _test_ui_diagnostics_layout_v3  # noqa: F401
 from .test_ui import router as test_ui_router
 from .test_ui_library_preview import router as test_ui_library_preview_router
 
